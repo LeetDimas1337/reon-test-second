c031a0609ed75d0fdad17bacbc7fe816cc4f96a4
LeetDimas1337 <dtaran.reon@gmail.com>
2023-08-21 16:25:08 +0300

    fix: добавил проверку на undefined для getDeal

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ import {DealHookBody, DealHookKeys, TypedRequestBody} from "./@types/CustomReque
 import api from "./api/api";
 import {getFieldValues} from "./utils";
 import {CustomFieldValue, DealCustomField} from "./@types/AmoTypes";
+import {CreatedTask} from "./types/task/task";
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const SERVICES_LIST_ID = '919003'
+const CHECK_PRICE_TASK_ID = 3035714
 
 const amo = new AmoCRM(config.SUB_DOMAIN, config.AUTH_CODE)
 const countDealPrice = (selectedServicesList: CustomFieldValue[], contactPrices: CustomField[]) => {
@@ -26,7 +28,6 @@ const countDealPrice = (selectedServicesList: CustomFieldValue[], contactPrices:
         if (!servicePrice) {
             return price
         }
-        // throw new Error("У контакта не указана стоимость одной из услуг")
         return price + Number(servicePrice.values[0].value)
     }, 0)
 }
@@ -35,16 +36,21 @@ const createTask = () => {
 
 }
 
-
 app.get('/', (req, res) => res.send("pong"))
 
 amo.getAccessToken().then(() => {
 
     app.post('/deal-hook', async (req: TypedRequestBody<DealHookBody>, res: Response) => {
         try {
-            const [deal] = (req.body.leads.update || req.body.leads.add)
-            const {contacts} = (await amo.getDeal(deal.id, ['contacts']))._embedded
-            const mainContact = contacts.find((contact: EmbeddedContact) => contact.is_main)
+
+            const [dealHook] = (req.body.leads.update || req.body.leads.add)
+            const deal = await amo.getDeal(dealHook.id, ['contacts'])
+
+            if (!deal._embedded) {
+                throw new Error("Не пришел список контактов")
+            }
+
+            const mainContact = deal._embedded.contacts?.find((contact: EmbeddedContact) => contact.is_main)
 
             if (!mainContact) {
                 throw new Error("К сделке не прикреплено ни одного контакта")
@@ -56,16 +62,18 @@ amo.getAccessToken().then(() => {
                 throw new Error("У контакта не указана стоимость услуг")
             }
 
-            const services = deal.custom_fields.find((customField) => customField.id === SERVICES_LIST_ID)
+            const services = dealHook.custom_fields.find((customField) => customField.id === SERVICES_LIST_ID)
 
             if (!services) {
-                await amo.updateDeal({id: Number(deal.id), price: 0})
+                await amo.updateDeal({id: Number(dealHook.id), price: 0})
                 return res.json({message: 'OK'})
             }
             const price = countDealPrice(services.values, contactData)
 
-            if (price !== Number(deal.price))
-                await amo.updateDeal({id: Number(deal.id), price})
+            if (price !== Number(dealHook.price)) {
+                await amo.updateDeal({id: Number(dealHook.id), price})
+                // await amo.createTasks([{task_type_id: CHECK_PRICE_TASK_ID, text: 'Проверить бюджет'} as CreatedTask])
+            }
 
             return res.json({message: 'OK'})
 
