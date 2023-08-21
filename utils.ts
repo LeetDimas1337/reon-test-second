e462b6719a7c8b4aeec267f64579dc741a4e3253
LeetDimas1337 <dtaran.reon@gmail.com>
2023-08-21 16:31:56 +0300

    fix: убрал закомментированный код и лишние методы

--- utils.ts
+++ utils.ts
@@ -1,18 +1,7 @@
 import moment from "moment";
-import fs from "fs";
 import {CustomField} from './types/customField/customField'
-// import { serverPatientSanation } from "../../IDENT/IDENT_v.4/server/@types/ident-patients-type";
-// import { SANATION_ENUM_ID } from "../../IDENT/IDENT_v.4/server/constants";
 
 const getTodayDateTime = (): string => moment().format("YYYY-MM-DD HH:MM:ss");
-
-const getClearPhoneNumber = (tel: string | undefined) => {
-    const clearNumber = !tel ? [] : tel.split("").filter(item => new RegExp(/\d/).test(item));
-    if (!clearNumber.length) {
-        return undefined;
-    }
-    return clearNumber.length > 10 ? clearNumber.join("").slice(1) : clearNumber.join("");
-};
 
 const getFieldValue = (customFields: Array<CustomField>, fieldId: number) => {
     const field = customFields
@@ -32,30 +21,6 @@ const getFieldValues = (customFields: Array<CustomField>, fieldId: number) => {
     return values.map(item => item.value);
 };
 
-// const getAllPages = async (request: ({page, limit}: requestParameters) => Promise<any>, page = 1, limit = 200) => {
-//     try {
-//         console.log(`Downloading page ${page}`);
-//         let res = await request({page, limit});
-//         let retry = 3;
-//         console.log("Quantity of elements", res.length)
-//         if (!res.length) {
-//             for (let i = 0; i < retry; i++) {
-//                 console.log("Array is empty, try to get page again")
-//                 res = await request({page, limit});
-//                 if (res.length === limit) {
-//                     break
-//                 }
-//             }
-//         }
-//         if (res.length === limit) {
-//             const next: any = await getAllPages(request, page + 1, limit);
-//             return [...res, ...next];
-//         }
-//         return res;
-//     } catch (e) {
-//         console.log(e);
-//     }
-// };
 
 const makeField = (field_id: number, value?: string | number | boolean, enum_id?: number) => {
     if (!value) {
@@ -90,115 +55,6 @@ const getDateUnixValue = (date: string) => {
     ).unix();
 };
 
-// const analyzePatientSanation = (patientSanation?: serverPatientSanation) => {
-//     if (!patientSanation) {
-//         return false
-//     }
-//     const {
-//         ID_TherapeuticSanations,
-//         ID_ProstheticSanations,
-//         ID_OrthodonticSanations,
-//         ID_ParodontolalSanations,
-//         ID_SurgicalSanations
-//     } = patientSanation;
-//     const sanationTypes = [ID_TherapeuticSanations, ID_ProstheticSanations, ID_OrthodonticSanations, ID_ParodontolalSanations, ID_SurgicalSanations];
-//     if (sanationTypes.some((element) => element === 2 || element === 3)) {
-//         return true
-//     }
-//     return false
-// };
-
-/**
- * Функция возвращает мультисписковое поле для карточки контакта "Информация о санации"
- */
-// const getSanationMultiselectFields = (field_id: number, patientSanation?: serverPatientSanation, sanationEnumIds = SANATION_ENUM_ID) => {
-//     if (!patientSanation) {
-//         return undefined
-//     }
-//
-//     const sanationTypes = [
-//         {
-//             name: "ID_TherapeuticSanations",
-//             value: patientSanation.ID_TherapeuticSanations
-//         },
-//         {
-//             name: "ID_ProstheticSanations",
-//             value: patientSanation.ID_ProstheticSanations
-//         },
-//         {
-//             name: "ID_OrthodonticSanations",
-//             value: patientSanation.ID_OrthodonticSanations
-//         },
-//         {
-//             name: "ID_ParodontolalSanations",
-//             value: patientSanation.ID_ParodontolalSanations
-//         },
-//         {
-//             name: "ID_SurgicalSanations",
-//             value: patientSanation.ID_SurgicalSanations
-//         }
-//     ];
-//
-//     return {
-//         field_id: field_id,
-//         values: sanationTypes.filter(element => element.value === 2 || element.value === 3).map(element => {
-//             return {
-//                 value: sanationEnumIds[element.name][1],
-//                 enum_id: sanationEnumIds[element.name][0]
-//             }
-//         })
-//     }
-// }
-
-//функция для разбиения запроса на создание на несколько по chunkSize
-// const bulkOperation = async (
-//     request: (data: any) => Promise<any>,
-//     data: Array<any>,
-//     chunkSize: number,
-//     operationName = "bulk"
-// ) => {
-//     let failed = [];
-//     if (data.length) {
-//         console.log(`Start operation of ${operationName}`);
-//         const result = [];
-//         try {
-//             const chunksCount = data.length / chunkSize;
-//             for (let i = 0; i < chunksCount; i++) {
-//                 try {
-//                     const sliced = data.slice(i * chunkSize, (i + 1) * chunkSize);
-//                     const requestResult: createdDealResponse[] = await request(sliced);
-//                     if (requestResult && requestResult.length > 0) {
-//                         const updatedResult = requestResult.map((element, index) => {
-//                             if (!i) {
-//                                 return {
-//                                     ...element, request_id: String(index),
-//                                 }
-//                             }
-//                             return {
-//                                 ...element, request_id: String((i * chunkSize) + index),
-//                             }
-//                         })
-//                         result.push(...updatedResult)
-//                     }
-//                 } catch (e) {
-//                     console.log(e)
-//                     failed.push(...data.slice(i * chunkSize, (i + 1) * chunkSize));
-//                 }
-//                 console.log(
-//                     `${operationName} ${i * chunkSize} - ${(i + 1) * chunkSize}`
-//                 );
-//             }
-//             return result;
-//         } catch (e) {
-//             console.log(e)
-//         }
-//     }
-//     console.log(
-//         `operation "${operationName}" finished. Failed - ${failed.length}`
-//     );
-//     fs.writeFileSync(`./bulkOperations_logs/${operationName}Failed.txt`, JSON.stringify(failed));
-// };
-
 const getUniqNumbers = (numbers: number[]): number[] => {
     const numberCollection = new Set();
     numbers.forEach((number) => numberCollection.add(number));
@@ -207,16 +63,11 @@ const getUniqNumbers = (numbers: number[]): number[] => {
 };
 
 export {
-    getClearPhoneNumber,
     getFieldValue,
     getFieldValues,
-    // getAllPages,
     makeField,
     getUnixBithdate,
     getDateUnixValue,
-    // analyzePatientSanation,
-    // getSanationMultiselectFields,
-    // bulkOperation,
     getTodayDateTime,
     getUniqNumbers,
     getHuminizeTimeFromUnix
