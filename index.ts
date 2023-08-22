6b305943ab1d8f08487d38e805bfcbe608cc7e34
LeetDimas1337 <dtaran.reon@gmail.com>
2023-08-22 10:34:29 +0300

    feat: создание задачи "Проверить бюджет", при условии, что она не создана ранее

--- index.ts
+++ index.ts
@@ -2,15 +2,11 @@ import express from "express";
 import AmoCRM from "./api/amo";
 import {mainLogger} from "./logger"
 import config from "./config";
-import {Request, Response} from "express";
+import {Response} from "express";
 import {CustomField} from "./types/customField/customField";
-import {AxiosResponse} from "axios";
 import {Contact as EmbeddedContact} from "./types/embeddedEntities/embeddedEntities";
-import {Contact} from "./types/contacts/contact"
-import {DealHookBody, DealHookKeys, TypedRequestBody} from "./@types/CustomRequest";
-import api from "./api/api";
-import {getFieldValues} from "./utils";
-import {CustomFieldValue, DealCustomField} from "./@types/AmoTypes";
+import {DealHookBody, TypedRequestBody} from "./@types/CustomRequest";
+import {CustomFieldValue} from "./@types/AmoTypes";
 import {CreatedTask} from "./types/task/task";
 
 const app = express();
@@ -20,7 +16,7 @@ app.use(express.urlencoded({extended: true}));
 
 const SERVICES_LIST_ID = '919003'
 const CHECK_PRICE_TASK_ID = 3035714
-
+const CHECK_TASK_TEXT = 'Проверить бюджет'
 const amo = new AmoCRM(config.SUB_DOMAIN, config.AUTH_CODE)
 const countDealPrice = (selectedServicesList: CustomFieldValue[], contactPrices: CustomField[]) => {
     return selectedServicesList.reduce((price, {value}) => {
@@ -32,9 +28,33 @@ const countDealPrice = (selectedServicesList: CustomFieldValue[], contactPrices:
     }, 0)
 }
 
-const createTask = () => {
+const checkAndCreateTask = async (dealId: string): Promise<void> => {
+
+    const newTaskData = {
+        entity_id: Number(dealId),
+        entity_type: 'leads',
+        task_type_id: CHECK_PRICE_TASK_ID,
+        text: CHECK_TASK_TEXT,
+        complete_till: getDeadlineTime(1)
+    } as CreatedTask
+
+    const tasks = await amo.getUnfulfilledTasksFromDeal(dealId)
+
+    if (typeof tasks === 'string') {
+        await amo.createTasks([newTaskData])
+    } else {
+        const checkTask = tasks._embedded.tasks.find(task => task.text === CHECK_TASK_TEXT)
+        if (!checkTask) {
+            await amo.createTasks([newTaskData])
+        }
+    }
 
 }
+
+const getDeadlineTime = (days: number): number => {
+    return Math.floor((Date.now() + days * 1000 * 3600 * 24) / 1000)
+}
+
 
 app.get('/', (req, res) => res.send("pong"))
 
@@ -56,7 +76,7 @@ amo.getAccessToken().then(() => {
                 throw new Error("К сделке не прикреплено ни одного контакта")
             }
 
-            const {custom_fields_values: contactData} = await amo.getContact(mainContact.id)
+            const {custom_fields_values: contactData} = await amo.getContact(String(mainContact.id))
 
             if (!contactData) {
                 throw new Error("У контакта не указана стоимость услуг")
@@ -72,7 +92,7 @@ amo.getAccessToken().then(() => {
 
             if (price !== Number(dealHook.price)) {
                 await amo.updateDeal({id: Number(dealHook.id), price})
-                // await amo.createTasks([{task_type_id: CHECK_PRICE_TASK_ID, text: 'Проверить бюджет'} as CreatedTask])
+                await checkAndCreateTask(dealHook.id)
             }
 
             return res.json({message: 'OK'})
@@ -82,7 +102,6 @@ amo.getAccessToken().then(() => {
         }
 
     })
-
 
     app.listen(config.PORT, () => {
         mainLogger.debug('Server started on', config.PORT)
